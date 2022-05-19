ac43841b8fc6f14021e68c51b941bced559c4ac7
Eric Batista Feitosa <eric.feitosa@kyndryl.com>
2022-05-18 21:44:48 -0300

    import React , {useState}

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react'
+import React , {useState} from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
